cba55fb4073745a5623f3e068948d0c62b5f2f3b
Mirko <mirkoat21@gmail.com>
2024-02-03 22:21:44 +0100

    kuce mace

--- Moj-Vite-Frizider/src/components/DIsplayComponent.jsx
+++ Moj-Vite-Frizider/src/components/DIsplayComponent.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect } from 'react';
+import '../styles/Recipes.css';
 
 function DisplayComponents(data) {
+    function foods(foods) {
+        return `
+      <h4>Favorite Foods</h4>
+      <ul class="foods-list">
+      ${foods.map(food => `<li>${food}</li>`).join("")}
+      </ul>
+      `;
+      }
+    
     useEffect(() => {
         const appElement = document.getElementById("app");
         if (appElement) {
-            if (Array.isArray(data)) {
+            if (1) {
                 appElement.innerHTML = `
                     <h1 class="app-title">Pets (${data.length} results)</h1>
-                    ${data.map(petTemplate).join("")}
+                    ${data.data.map(petTemplate).join("")}
                     <p class="footer">These ${data.length} pets were added recently. Check back soon for updates.</p>
                 `;
             } else {
@@ -23,7 +33,7 @@ function DisplayComponents(data) {
             <div class="animal">
                 <img class="pet-photo" src="${data.photo}">
                 <h2 class="pet-name">${data.name} <span class="species">(${data.species})</span></h2>
-                <p><strong>Age:</strong> ${age(data.birthYear)}</p>
+                <p><strong>Age:</strong> ${data.birthYear}</p>
                 ${data.favFoods ? foods(data.favFoods) : ""}
             </div>
         `;
@@ -31,7 +41,7 @@ function DisplayComponents(data) {
 
     return (
         <>
-            <div id="app"></div>
+            <div id="app" className="display"></div>
         </>
     );
 }
