52dfd185acd657c5ece0ae5980f447bb9e8366e6
unknown <stefana.miladinovic@elfak.rs>
2024-02-11 04:44:06 +0100

    updateRecipe

--- Moj-Vite-Frizider/src/pages/DetailedRecipe.jsx
+++ Moj-Vite-Frizider/src/pages/DetailedRecipe.jsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/DetailedRecipe.css';
+import DisplayComponent from "../components/DIsplayComponent"
 
 function DetailedRecipe() {
     const [recommendations, setRecommendations] = useState([]);
+    const [recipeCuisine, setRecipeCuisine] = useState('');
+    const [recipeChef, setRecipeChef] = useState('');
+    const [recipeIngredients, setRecipeIngredients] = useState([]);
     const navigate = useNavigate();
 
     const handleAuthorClick = () => 
     {
         console.log("Author clicked");
-        sessionStorage.setItem('cheff', JSON.stringify(Recipe));
-        navigate(`/cheffportfolio/${Recipe.cheff}`);
+        //sessionStorage.setItem('cheff', JSON.stringify(Recipe));
+        //navigate(`/cheffportfolio/${Recipe.cheff}`);
     }
 
     const goBack = () =>
     {
+        sessionStorage.removeItem('recipe');
         navigate(`/fridge`);
     }
 
@@ -25,6 +30,56 @@ function DetailedRecipe() {
     }, []);
 
     const Recipe = JSON.parse(sessionStorage.getItem('recipe'));
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/cuisine/${Recipe.cuisine}`)
+       .then(response => response.json())
+       .then(data => {
+           setRecipeCuisine(data);
+       })
+       .catch(error => {
+           // Handle error if fetch fails
+           console.error('Error fetching cuisine:', error);
+       });
+       fetch(`http://localhost:3000/user/byid/${Recipe.chef}`)
+       .then(response => response.json())
+       .then(data => {
+           setRecipeChef(data);
+       })
+       .catch(error => {
+           // Handle error if fetch fails
+           console.error('Error fetching cuisine:', error);
+       });
+   }, []);
+
+   useEffect(() => {
+    handleFind()
+        .then(ingredients => {
+            setRecipeIngredients(ingredients);
+            console.log(ingredients);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+ }, [recipeCuisine]);
+    const handleFind = async () => {
+        const promises = Recipe.ingredients.map(ingredientId => {
+            return fetch(`http://localhost:3000/ingredient/${ingredientId}`)
+                .then(response => response.json())
+                .then(data => data.name)
+                .catch(error => {
+                    console.error('Error fetching ingredients:', error);
+                    return ''; // Return empty string in case of an error
+                });
+        });
+
+        return Promise.all(promises);
+    };
+
+    const goToUpdatePage = () =>
+    {
+        navigate(`/createRecipe/${Recipe.name}`);
+    }
 
     const Comments = [
      {
@@ -57,24 +112,33 @@ function DetailedRecipe() {
     return (
         <div className="details-all">
             <button className="details-button edge" onClick={() => goBack()}>Back</button>
+           {Recipe.chef == sessionStorage.getItem('userId') && (
+            <button className="details-button edge" onClick={() => goToUpdatePage()}>Update</button>
+            )}
             <div className="main-header">
-                <img src={Recipe.photo} alt="There was no photo for this recipe" className="thumb" />
+                <img src={Recipe.imageURL} alt="There was no photo for this recipe" className="thumb" />
                 <div className="about">
-                    <p className="title-desc">{Recipe.name} <span className="cuisine">({Recipe.cuisine})</span></p><br/>
-                    <u className="tags">{Recipe.category}<span>,</span>{Recipe.cookingType}</u><br/>
-                    <p className="description-long">{Recipe.description}</p><br/>
-                    <p className="money">Total budget cost: <span className="the-green">{Recipe.budget}</span></p><br/>
-                    <a className="author" onClick={() => handleAuthorClick()}> - by {Recipe.cheff}</a>
+                    <p className="title-desc">{Recipe.name} <span className="cuisine">({recipeCuisine.name})</span></p><br/>
+                    <p className="tags"><strong>Cooking type: </strong><u >{Recipe.cookingType}</u></p>
+                    <p className="tags"><strong>Categoties: </strong><u >{Recipe.category.join(",")}</u></p>
+                    <p className="money"><strong>Budget: </strong><span className="the-green">{Recipe.budget}</span></p>
+                    <h4 className="tags">Ingredients:</h4>
+                            <ul> 
+                                  {recipeIngredients.map(food => <li>{food}</li>)}
+                            </ul>
+                    <a className="author" onClick={() => handleAuthorClick()}> - by {recipeChef.name}</a>
                 </div>
             </div>
             <div className="lists">
                 <div className="list">
-                    <p className="list-title">Ingredients:</p>
+                   {/* <p className="list-title">Ingredients:</p>
                     <p className="list-items">
-                        {Recipe.ingredients.map(ingredient => (
-                            <span>{ingredient}, </span>
-                        ))}
-                    total of <span className="total">{Recipe.ingredients.length}</span> ingredients.</p>
+                        {recipeIngredients}
+                    total of <span className="total">{Recipe.ingredients.length}</span> ingredients.</p>*/}
+                    <p className="list-title">Preparation process:</p>
+                    <div className="description-long" >
+                        <p>{Recipe.description}</p><br/>
+                    </div>
                 </div>
                 <div className="list">
                     <p className="list-title">Loved by:</p>
